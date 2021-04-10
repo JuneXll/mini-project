ed43b60093d5258bdfe2761208ecb45a78d0ed43
Jvala2 <joevalancy@gmail.com>
2021-04-10 14:02:03 -0400

    Added js code for retriving from api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,32 +1,19 @@
+var search=document.getElementById("search").value;
 
+var format=document.getElementById("dropdown").value;
 
+var requestUrl="https://www.loc.gov/"+format+"/?q="+search+"&fo=json"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var requestUrl="https://libraryofcongress.github.io/data-exploration/"
-
-fetch(requestUrl).then(function(response) {
-    if(!response.ok) {
-        document.getElementById("searchBar".innerText="Error, invalid, or Joseph just futzed the code.");
-    }
-    return response.json();
-}).then(function(data) {
-    console.log(data);
-
+document.getElementById("search").addEventListener("submit",function(event) {
+    event.preventDefault();
     
-})
+
+//This part checks if the input is correct.
+    fetch(requestUrl).then(function(response) {
+        if(!response.ok) {
+            document.getElementById(/*Add a place for an error message in body*/);
+        } 
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    });})
